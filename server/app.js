927942ba4cfa953f882ce9536680d71053daab0c
samvelie <sam.velie@gmail.com>
2017-02-22 11:34:46 -0600

    serving static files

--- server/app.js
+++ server/app.js
@@ -1,1 +1,8 @@
 console.log('Starting the server!!!!');
+
+var express = require('express'); //this takes in the express folder which returns a function
+var app = express();    //calling this function returns an object with many useful properties
+
+app.use(express.static('server/public'));
+
+app.listen(5000);
