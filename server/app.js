7e9f3cbc694830d0a7d1317b708028ae1d44791b
samvelie <sam.velie@gmail.com>
2017-02-22 15:21:57 -0600

    get requests working

--- server/app.js
+++ server/app.js
@@ -5,4 +5,28 @@ var app = express();    //calling this function returns an object with many usef
 
 app.use(express.static('server/public'));
 
+var fishiesList = [{name: 'walleye'}, {name: 'pike'}, {name: 'muskie'}];
+
+app.get('/fish', function(req, res){
+  console.log('req:', req);
+  res.send(fishiesList);
+});
+
+app.get('/fish/first', function(req, res){
+  res.send(fishiesList[0]);
+});
+
+app.get('/fish/first/name', function(req, res){
+  res.send(fishiesList[0].name);
+});
+
+app.get('/fish/last', function(req, res){
+  res.send(fishiesList[fishiesList.length-1]);
+});
+
+app.get('/fish/last/name', function(req, res){
+  var lastFishIndex = fishiesList.length-1;
+  res.send(fishiesList[lastFishIndex].name);
+});
+
 app.listen(5000);
--- server/public/client.js
+++ server/public/client.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+    console.log('jquery sourced');
+    $.ajax({
+      type: 'GET',
+      url: '/fish',
+      success: function(response){
+        console.log(response);
+        for (var i = 0; i < response.length; i++) {
+          $('#fishTank').append('<li>' + response[i].name + '</li>');
+        }
+      }//end success function
+    })//end ajax function
 
-  console.log('jquery sourced');
-})
+    $.ajax({
+      type: 'GET',
+      url: '/fish/first/name',
+      success: function(response){
+        console.log(response);
+        $('#firstFishy').text(response);
+      }//end success function
+    })//end ajax function
+});//end document ready
